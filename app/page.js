0a1b8a71bcc205b47d93b5a0e89ac9892e981df3
Wesley Hitson <wesleyhitson106@gmail.com>
2024-06-25 16:30:46 -0400

    finish course

---
+++ app/page.js
@@ -0,0 +1,21 @@
+import LikeButton from './like-button';
+ 
+function Header({ title }) {
+  return <h1>{title ? title : 'Default title'}</h1>;
+}
+ 
+export default function HomePage() {
+  const names = ['Ringo Starr', 'Paul McCartney', 'John Lennon', 'George Harrison'];
+ 
+  return (
+    <div>
+      <Header title="The Bottles" />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+      <LikeButton />
+    </div>
+  );
+}
